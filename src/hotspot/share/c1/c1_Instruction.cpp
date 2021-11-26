eb18f731ba0599446dfc482c113ecabfa3c30907
Ludvig Janiuk <ludvig.j.janiuk@oracle.com>
2021-11-26 14:19:47 +0100

    disconnect from graph nicer

--- src/hotspot/share/c1/c1_Instruction.cpp
+++ src/hotspot/share/c1/c1_Instruction.cpp
@@ -525,6 +525,7 @@ Constant::CompareResult Constant::compare(Instruction::Condition cond, Value rig
 // Implementation of BlockBegin
 
 void BlockBegin::set_end(BlockEnd* new_end) {
+  // Assumes that no
   assert(new_end != NULL, "Should not reset block new_end to NULL");
   if (new_end == _end) return;
 
